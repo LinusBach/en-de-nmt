b46c531eb07f824344f0693b4c3af1550eb59897
BlueJ1 <l.august@posteo.net>
2023-06-09 14:18:01 +0200

    New

--- decoder.py
+++ decoder.py
@@ -3,27 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class DecoderRNNSimple(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNNSimple, self).__init__()
-        self.hidden_size = hidden_size
-
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, features, hidden):
-        output = self.embedding(features).view(1, 1, -1)
-        output = F.relu(output)
-        output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
-        return output, hidden
-
-    def init_hidden(self, device="cpu"):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
-
-
 class AttnDecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=None):
         super(AttnDecoderRNN, self).__init__()
--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from evaluate import evaluate
 from dataloader import *
 import random
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu" if torch.backends.mps.is_available() else "cpu")
 
-teacher_forcing_ratio = 0.5
+teacher_forcing_ratio = 1
 
-epochs = 200
-n_samples = 100
+epochs = 500
+n_samples = 1000
 
 input_lang, output_lang, pairs = prepare_data('data/train.en', 'data/train.de', n_samples)
 print(f'number of pairs: {len(pairs)}')
 
-hidden_size = 256
+hidden_size = 512
 encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
 attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1, max_length=MAX_LENGTH).to(device)
 
