dea1dd24576f6bbafba87b4c268a60c9da613556
BlueJ1 <l.august@posteo.net>
2023-06-15 13:13:52 +0200

    .

--- plot.py
+++ plot.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import os
 
 
-def plot(points, step_size, plots_dir="plots", model_name="model"):
+def plot_training(points, step_size, plots_dir="plots", model_name="model"):
     if not os.path.exists(plots_dir):
         os.mkdir(plots_dir)
 
     plt.figure()
-    plt.plot(list(range(step_size, (1 + len(points)) * step_size, step_size)), points)
+    plt.plot(list(range(0, len(points) * step_size, step_size)), points)
     plt.savefig("plots/" + model_name + ".png")
     plt.close()
